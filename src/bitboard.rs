0fc68c0ab1d7f970103209c06d9d1adadf1f0903
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-01-30 18:58:00 +0100

    Bitboards

---
+++ src/bitboard.rs
@@ -0,0 +1,184 @@
+use std::ops;
+use std::num;
+use std::fmt;
+
+use square::Square;
+
+#[derive(PartialEq, Eq, Copy, Clone)]
+pub struct Bitboard(pub u64);
+
+impl Bitboard {
+    pub fn from_square(Square(sq): Square) -> Bitboard {
+        Bitboard(1 << sq)
+    }
+
+    pub fn all() -> Bitboard {
+        Bitboard(!0)
+    }
+
+    pub fn rank(rank: i8) -> Bitboard {
+        Bitboard(0xff << rank)
+    }
+
+    pub fn file(file: i8) -> Bitboard {
+        Bitboard(0x101010101010101 << file)
+    }
+
+    pub fn is_empty(self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn contains(self, sq: Square) -> bool {
+        !(self & Bitboard::from_square(sq)).is_empty()
+    }
+
+    pub fn add(&mut self, Square(sq): Square) {
+        self.0 |= 1 << sq;
+    }
+
+    pub fn lsb(self) -> Option<Square> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(Square(self.0.trailing_zeros() as i8))
+        }
+    }
+
+    #[cfg(target_feature="bmi2")]
+    pub fn pext(self, Bitboard(mask): Bitboard) -> u64 {
+        let Bitboard(src) = self;
+        let result: u64;
+        unsafe {
+            asm!("pextq $2, $0, $0"
+                 : "=r"(result)
+                 : "0"(src), "r"(mask));
+        }
+        result
+    }
+
+    #[cfg(not(target_feature="bmi2"))]
+    pub fn pext(self, Bitboard(mut mask): Bitboard) -> u64 {
+        let Bitboard(src) = self;
+        let mut result = 0;
+        let mut bit = 0;
+
+        while mask != 0 {
+            if src & mask & 0u64.wrapping_sub(mask) != 0 {
+                result = result | bit;
+            }
+
+            mask = mask & mask.wrapping_sub(1);
+            bit <<= 1;
+        }
+
+        result
+    }
+
+    pub fn subsets(self) -> CarryRippler {
+        CarryRippler::new(self)
+    }
+}
+
+impl num::Zero for Bitboard {
+    fn zero() -> Bitboard {
+        Bitboard(0)
+    }
+}
+
+impl fmt::Debug for Bitboard {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for rank in (0..8).rev() {
+            for file in 0..8 {
+                if self.contains(Square::new(file, rank)) {
+                    write!(f, "1");
+                } else {
+                    write!(f, ".");
+                }
+
+                if file < 7 {
+                    write!(f, " ");
+                } else {
+                    write!(f, "\n");
+                }
+            }
+        }
+
+        return write!(f, ""); // TODO
+    }
+}
+
+impl ops::BitAnd<Bitboard> for Bitboard {
+    type Output = Bitboard;
+
+    fn bitand(self, Bitboard(rhs): Bitboard) -> Bitboard {
+        let Bitboard(lhs) = self;
+        Bitboard(lhs & rhs)
+    }
+}
+
+impl ops::BitOr<Bitboard> for Bitboard {
+    type Output = Bitboard;
+
+    fn bitor(self, Bitboard(rhs): Bitboard) -> Bitboard {
+        let Bitboard(lhs) = self;
+        Bitboard(lhs | rhs)
+    }
+}
+
+impl ops::Not for Bitboard {
+    type Output = Bitboard;
+
+    fn not(self) -> Bitboard {
+        Bitboard(!self.0)
+    }
+}
+
+impl Iterator for Bitboard {
+    type Item = Square;
+
+    fn next(&mut self) -> Option<Square> {
+        let square = self.lsb();
+        self.0 &= self.0.wrapping_sub(1);
+        square
+    }
+}
+
+struct CarryRippler {
+    bb: u64,
+    subset: u64,
+    first: bool,
+}
+
+impl CarryRippler {
+    fn new(Bitboard(bb): Bitboard) -> CarryRippler {
+        CarryRippler { bb: bb, subset: 0, first: true }
+    }
+}
+
+impl Iterator for CarryRippler {
+    type Item = Bitboard;
+
+    fn next(&mut self) -> Option<Bitboard> {
+        let subset = self.subset;
+        if self.first || subset != 0 {
+            self.first = false;
+            self.subset = self.subset.wrapping_sub(self.bb) & self.bb;
+            Some(Bitboard(subset))
+        } else {
+            None
+        }
+    }
+}
+
+mod test {
+    use std::num::Zero;
+    use bitboard::Bitboard;
+    use square;
+
+    #[test]
+    fn test_lsb() {
+        assert_eq!(Bitboard::from_square(square::A1).lsb(), Some(square::A1));
+        assert_eq!(Bitboard::from_square(square::D2).lsb(), Some(square::D2));
+        assert_eq!(Bitboard::zero().lsb(), None);
+    }
+}
