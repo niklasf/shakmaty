b160e68794fff898d5c4d5b3d9dbc6ec900fe715
Lakin Wecker <lakin.wecker@gmail.com>
2017-02-15 10:58:04 -0700

    adding a lib version as well

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![feature(associated_consts)]
+#![feature(cfg_target_feature)]
+#![feature(asm)]
+#![feature(field_init_shorthand)]
+
+pub mod square;
+pub mod types;
+pub mod bitboard;
+pub mod attacks;
+pub mod board;
