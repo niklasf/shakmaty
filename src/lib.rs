e4559a8b37acf42c3490051a372e6670802a0076
Lakin Wecker <lakin@structuredabstraction.com>
2017-03-06 10:53:04 -0700

    Bringing the lib up to date with the src/main.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+#![feature(test)]
 #![feature(associated_consts)]
 #![feature(cfg_target_feature)]
-#![feature(asm)]
 #![feature(field_init_shorthand)]
+#![feature(platform_intrinsics)]
+
+extern crate rayon;
 
 pub mod square;
 pub mod types;
 pub mod bitboard;
 pub mod attacks;
 pub mod board;
+pub mod position;
