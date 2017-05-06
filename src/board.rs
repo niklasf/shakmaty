a6143543e1ffb3983f3a6bc7f4e9ac0b85565606
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-05-06 21:46:47 +0200

    Further reduce lazy_static cost

--- src/board.rs
+++ src/board.rs
@@ -324,8 +324,9 @@ impl Board {
     }
 
     pub fn attacks_to(&self, sq: Square) -> Bitboard {
-        (attacks::rook_attacks(sq, self.occupied) & (self.rooks | self.queens)) |
-        (attacks::bishop_attacks(sq, self.occupied) & (self.bishops | self.queens)) |
+        let precomp = &attacks::PRECOMP;
+        (precomp.rook_attacks(sq, self.occupied) & (self.rooks | self.queens)) |
+        (precomp.bishop_attacks(sq, self.occupied) & (self.bishops | self.queens)) |
         (attacks::knight_attacks(sq) & self.knights) |
         (attacks::king_attacks(sq) & self.kings) |
         (attacks::pawn_attacks(Color::White, sq) & self.pawns & self.black) |
