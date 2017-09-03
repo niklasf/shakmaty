ba7e711d199287b6ce0f2656e1ef626c1b22f125
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-09-03 09:26:34 +0200

    Tweak remove_piece_at

--- src/board.rs
+++ src/board.rs
@@ -154,7 +154,12 @@ impl Board {
     #[inline]
     pub fn remove_piece_at(&mut self, sq: Square) -> Option<Piece> {
         let piece = self.piece_at(sq);
-        self.discard_piece_at(sq);
+        piece.map(|p| {
+            self.occupied.flip(sq);
+            self.by_color_mut(p.color).flip(sq);
+            self.by_role_mut(p.role).flip(sq);
+            self.promoted.discard(sq);
+        });
         piece
     }
 
