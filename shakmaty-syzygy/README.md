6c0909834ff6f7b9bd272e3f237ddb37251e0a5e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-10-03 16:06:31 +0200

    start out with Cargo.toml

---
+++ shakmaty-syzygy/README.md
@@ -0,0 +1,23 @@
+shakmaty-syzygy
+===============
+
+A Rust library for probing Syzygy tablebases
+
+[![Build Status](https://travis-ci.org/niklasf/shakmaty-syzygy.svg?branch=master)](https://travis-ci.org/niklasf/shakmaty-syzygy)
+[![crates.io](https://img.shields.io/crates/v/shakmaty-syzygy.svg)](https://crates.io/crates/shakmaty-syzygy)
+
+Documentation
+-------------
+
+[Read the documentation](https://docs.rs/shakmaty-syzygy)
+
+Changelog
+
+* Upcoming
+  - First release.
+
+License
+-------
+
+shakmaty-syzygy is licensed under the GPL-3.0 (or any later version at your
+option). See the COPYING file for the full license text.
---
+++ shakmaty-syzygy/src/lib.rs
@@ -0,0 +1,21 @@
+// This file is part of the shakmaty-syzygy library.
+// Copyright (C) 2017 Niklas Fiekas <niklas.fiekas@backscattering.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+//! Probe Syzygy tablebases.
+
+#![doc(html_root_url = "https://docs.rs/shakmaty-syzygy/0.1.0")]
+
+#![warn(missing_debug_implementations)]
