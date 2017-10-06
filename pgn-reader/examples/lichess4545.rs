0ea447678755d5970b0c21cee88fbe9e7cd0a939
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-10-06 12:39:37 +0200

    update season 9 participants

--- pgn-reader/examples/lichess4545.rs
+++ pgn-reader/examples/lichess4545.rs
@@ -24,174 +24,54 @@ impl<'a> ParticipantFilter<'a> {
     fn lichess4545_season_9() -> ParticipantFilter<'a> {
         let mut participants = HashSet::new();
 
-        participants.insert(unicase::Ascii::new("felixnl"));
-        participants.insert(unicase::Ascii::new("badplayer_cm"));
-        participants.insert(unicase::Ascii::new("mariontinsley"));
-        participants.insert(unicase::Ascii::new("toj"));
-        participants.insert(unicase::Ascii::new("archone"));
-        participants.insert(unicase::Ascii::new("chill5555"));
-        participants.insert(unicase::Ascii::new("boviced"));
-        participants.insert(unicase::Ascii::new("nicholasdrakes"));
-        participants.insert(unicase::Ascii::new("dmathman"));
-        participants.insert(unicase::Ascii::new("dracoxvitae"));
-        participants.insert(unicase::Ascii::new("jmhwsm"));
-        participants.insert(unicase::Ascii::new("mrjv"));
-        participants.insert(unicase::Ascii::new("eie24"));
-        participants.insert(unicase::Ascii::new("mebeka"));
-        participants.insert(unicase::Ascii::new("oignons"));
-        participants.insert(unicase::Ascii::new("tmaus"));
-        participants.insert(unicase::Ascii::new("be0wulf"));
-        participants.insert(unicase::Ascii::new("rpimpulse"));
-        participants.insert(unicase::Ascii::new("pronew"));
-        participants.insert(unicase::Ascii::new("david-innes"));
-        participants.insert(unicase::Ascii::new("deepblueincarnate"));
-        participants.insert(unicase::Ascii::new("mooserohde"));
-        participants.insert(unicase::Ascii::new("psimaster"));
-        participants.insert(unicase::Ascii::new("forhavu"));
-        participants.insert(unicase::Ascii::new("gorathger"));
-        participants.insert(unicase::Ascii::new("rb52"));
-        participants.insert(unicase::Ascii::new("fisher56"));
-        participants.insert(unicase::Ascii::new("outerheaven92"));
-        participants.insert(unicase::Ascii::new("pioki"));
-        participants.insert(unicase::Ascii::new("getagripwhitby"));
-        participants.insert(unicase::Ascii::new("sirturtle"));
-        participants.insert(unicase::Ascii::new("roso97"));
-        participants.insert(unicase::Ascii::new("rescinded"));
-        participants.insert(unicase::Ascii::new("udaysatya"));
-        participants.insert(unicase::Ascii::new("piotor"));
-        participants.insert(unicase::Ascii::new("napoleon_solo"));
-        participants.insert(unicase::Ascii::new("tipau"));
-        participants.insert(unicase::Ascii::new("s2004k1993"));
-        participants.insert(unicase::Ascii::new("checkmonk"));
-        participants.insert(unicase::Ascii::new("exoprimal"));
-        participants.insert(unicase::Ascii::new("tellum"));
-        participants.insert(unicase::Ascii::new("gucelli"));
-        participants.insert(unicase::Ascii::new("toffeeman"));
-        participants.insert(unicase::Ascii::new("wolf21"));
-        participants.insert(unicase::Ascii::new("truesacrifice"));
-        participants.insert(unicase::Ascii::new("staincastle"));
-        participants.insert(unicase::Ascii::new("glad_he_ate_her"));
-        participants.insert(unicase::Ascii::new("flandoo"));
-        participants.insert(unicase::Ascii::new("razorboy"));
-        participants.insert(unicase::Ascii::new("lelouch_vi_brittania"));
-        participants.insert(unicase::Ascii::new("benedictine"));
-        participants.insert(unicase::Ascii::new("corridor"));
-        participants.insert(unicase::Ascii::new("servasky"));
-        participants.insert(unicase::Ascii::new("jivey"));
-        participants.insert(unicase::Ascii::new("mdpotts22"));
-        participants.insert(unicase::Ascii::new("anunzio"));
-        participants.insert(unicase::Ascii::new("fradtheimpaler"));
-        participants.insert(unicase::Ascii::new("sgis"));
-        participants.insert(unicase::Ascii::new("spuntachessts"));
-        participants.insert(unicase::Ascii::new("nojero"));
-        participants.insert(unicase::Ascii::new("hetraie"));
-        participants.insert(unicase::Ascii::new("lichie"));
-        participants.insert(unicase::Ascii::new("pushedpawn"));
-        participants.insert(unicase::Ascii::new("kjar"));
-        participants.insert(unicase::Ascii::new("daveyjones01"));
-        participants.insert(unicase::Ascii::new("theratrivertrapper"));
-        participants.insert(unicase::Ascii::new("gogoyubari"));
-        participants.insert(unicase::Ascii::new("dimitris22"));
-        participants.insert(unicase::Ascii::new("delpire"));
-        participants.insert(unicase::Ascii::new("michael_hackett"));
-        participants.insert(unicase::Ascii::new("oddskill"));
-        participants.insert(unicase::Ascii::new("rhohit"));
-        participants.insert(unicase::Ascii::new("iongler"));
-        participants.insert(unicase::Ascii::new("wardsstone"));
-        participants.insert(unicase::Ascii::new("nombringer"));
-        participants.insert(unicase::Ascii::new("quirked"));
-        participants.insert(unicase::Ascii::new("isaypotato"));
-        participants.insert(unicase::Ascii::new("dahdah"));
-        participants.insert(unicase::Ascii::new("raitonvsfuuton"));
-        participants.insert(unicase::Ascii::new("johnsonbronson"));
-        participants.insert(unicase::Ascii::new("seb32"));
-        participants.insert(unicase::Ascii::new("yungpirc"));
-        participants.insert(unicase::Ascii::new("mamaduck"));
-        participants.insert(unicase::Ascii::new("cpapa14"));
-        participants.insert(unicase::Ascii::new("erinyu"));
-        participants.insert(unicase::Ascii::new("kobol"));
-        participants.insert(unicase::Ascii::new("lord_axe"));
-        participants.insert(unicase::Ascii::new("cosmosg"));
-        participants.insert(unicase::Ascii::new("toni4127"));
-        participants.insert(unicase::Ascii::new("aljopeljhan"));
-        participants.insert(unicase::Ascii::new("shetoo"));
-        participants.insert(unicase::Ascii::new("atil4"));
-        participants.insert(unicase::Ascii::new("tcgrif"));
-        participants.insert(unicase::Ascii::new("johnjpershing"));
-        participants.insert(unicase::Ascii::new("joeymellberg"));
-        participants.insert(unicase::Ascii::new("arkensiel"));
-        participants.insert(unicase::Ascii::new("kferapont"));
-        participants.insert(unicase::Ascii::new("hicetnunc"));
-        participants.insert(unicase::Ascii::new("mingpro"));
-        participants.insert(unicase::Ascii::new("somethingpretentious"));
-        participants.insert(unicase::Ascii::new("lynnpv"));
-        participants.insert(unicase::Ascii::new("revoof"));
-        participants.insert(unicase::Ascii::new("pokerram"));
-        participants.insert(unicase::Ascii::new("scarff"));
-        participants.insert(unicase::Ascii::new("michaelpren95"));
-        participants.insert(unicase::Ascii::new("immortality"));
-        participants.insert(unicase::Ascii::new("donkey-kong16"));
-        participants.insert(unicase::Ascii::new("rsava"));
-        participants.insert(unicase::Ascii::new("artykom"));
-        participants.insert(unicase::Ascii::new("mariuseg"));
-        participants.insert(unicase::Ascii::new("captncarter"));
-        participants.insert(unicase::Ascii::new("krispl"));
-        participants.insert(unicase::Ascii::new("cftsoc3"));
-        participants.insert(unicase::Ascii::new("foxxxy_coxxxy"));
-        participants.insert(unicase::Ascii::new("hackov"));
-        participants.insert(unicase::Ascii::new("ed84"));
-        participants.insert(unicase::Ascii::new("chess_patzer"));
-        participants.insert(unicase::Ascii::new("luvgangster"));
-        participants.insert(unicase::Ascii::new("nadgob"));
-        participants.insert(unicase::Ascii::new("noklu"));
-        participants.insert(unicase::Ascii::new("fabinou"));
-        participants.insert(unicase::Ascii::new("foreverweak"));
-        participants.insert(unicase::Ascii::new("chennis"));
-        participants.insert(unicase::Ascii::new("marstem"));
-        participants.insert(unicase::Ascii::new("slaiyn"));
-        participants.insert(unicase::Ascii::new("infernaljumble"));
-        participants.insert(unicase::Ascii::new("mird113"));
-        participants.insert(unicase::Ascii::new("flaxl"));
-        participants.insert(unicase::Ascii::new("dinomoomoo"));
-        participants.insert(unicase::Ascii::new("dose7781"));
-        participants.insert(unicase::Ascii::new("slavy"));
-        participants.insert(unicase::Ascii::new("wargoblin"));
-        participants.insert(unicase::Ascii::new("revhas"));
-        participants.insert(unicase::Ascii::new("tomek188"));
-        participants.insert(unicase::Ascii::new("chessanalyst"));
-        participants.insert(unicase::Ascii::new("onbion"));
-        participants.insert(unicase::Ascii::new("yago666"));
-        participants.insert(unicase::Ascii::new("loukas435"));
-        participants.insert(unicase::Ascii::new("tnan123"));
-        participants.insert(unicase::Ascii::new("ruizbr"));
-        participants.insert(unicase::Ascii::new("spiteknight"));
-        participants.insert(unicase::Ascii::new("zharptytsia"));
-        participants.insert(unicase::Ascii::new("zeikex"));
-        participants.insert(unicase::Ascii::new("john465"));
-        participants.insert(unicase::Ascii::new("lovlas"));
-        participants.insert(unicase::Ascii::new("noluckonlyskill"));
-        participants.insert(unicase::Ascii::new("gnarlygoat"));
-        participants.insert(unicase::Ascii::new("f1nn33"));
-        participants.insert(unicase::Ascii::new("prune2000"));
-        participants.insert(unicase::Ascii::new("vadsamoht"));
-        participants.insert(unicase::Ascii::new("odyn1982"));
-        participants.insert(unicase::Ascii::new("jpokerflat"));
-        participants.insert(unicase::Ascii::new("arex"));
-        participants.insert(unicase::Ascii::new("tsatsa64"));
-        participants.insert(unicase::Ascii::new("kylecuver1"));
-        participants.insert(unicase::Ascii::new("dank-chessessities"));
-        participants.insert(unicase::Ascii::new("alegre_river"));
-        participants.insert(unicase::Ascii::new("moistvonlipwig"));
-        participants.insert(unicase::Ascii::new("matt_p_14"));
-        participants.insert(unicase::Ascii::new("jg777"));
-        participants.insert(unicase::Ascii::new("axp156"));
-        participants.insert(unicase::Ascii::new("robinhood76"));
-        participants.insert(unicase::Ascii::new("materix235"));
-        participants.insert(unicase::Ascii::new("hoikka"));
-        participants.insert(unicase::Ascii::new("tothecloid"));
-        participants.insert(unicase::Ascii::new("gatorricky"));
-        participants.insert(unicase::Ascii::new("narwhalepete"));
-        participants.insert(unicase::Ascii::new("vevochi"));
+        for &participant in ["Alegre_River", "Anunzio", "Archone", "Arkensiel",
+            "Axp156", "Benedictine", "Boviced", "CapKas", "CarlosMagnussen",
+            "ChessBuzz17", "Chess_Patzer", "Corridor", "Daveyjones01",
+            "Delpire", "Dmathman", "Doevert", "Donkey-Kong16", "DracoxVitae",
+            "DzeiTi", "EXOprimal", "ErinYu", "Fabinou", "Flandoo",
+            "GetAGripWhitby", "Glad_He_Ate_Her", "GnarlyGoat", "GogoYubari",
+            "Hackov", "Hoikka", "Immortality", "InfernalJumble", "JMHWSM",
+            "JoeyMellberg", "JohnJPershing", "JohnsonBronson", "Jubka",
+            "KFerapont", "Kjar", "Kraaft", "LeDarkKnight",
+            "Lelouch_Vi_Brittania", "Lord_axe", "Lukhas", "Lynnpv", "MMichael",
+            "MarionTinsley", "MasterSalomon", "Materix235", "Mebeka",
+            "MichaelPren95", "Michael_hackett", "MoistvonLipwig",
+            "Mooserohde", "MrLegilimens", "NOTFOXAnonymous", "Nairwolf",
+            "Napoleon_Solo", "NarwhalePete", "NoKlu", "NoLuckOnlySkill",
+            "Nombringer", "OuterHeaven92", "Petruchio", "Pewaps",
+            "Plasterhippy", "PsiMaster", "Rascar_Capac", "RazorBoy", "Redban",
+            "Rescinded", "RodrigoDiazdeVivar", "Ronsaki", "RottenPawn",
+            "RuizBR", "Saxton47", "Scrooge", "Seb32", "Servasky", "Shetoo",
+            "SirTurtle", "Somethingpretentious", "Sonata2", "SpiteKnight",
+            "Staincastle", "TCGRIF", "TMaus", "Tellum", "TheRatRiverTrapper",
+            "Toffeeman", "Tsatsa64", "Vevochi", "Wolf21", "Zharptytsia",
+            "alexiov", "aliquantus", "aljopeljhan", "arex", "artykom",
+            "atil4", "badplayer_cm", "be0wulf", "cftsoc3", "checkmonk",
+            "chennis", "chessanalyst", "chesshavoc", "chesswithcoach",
+            "chill5555", "comped", "cpapa14", "dahdah", "dank-chessessities",
+            "david-innes", "deanmadden", "dimitris22", "dinomoomoo", "dkillian",
+            "dose7781", "ed84", "eie24", "f1nn33", "ffenliv", "fisher56",
+            "flaxl", "foon", "foreverweak", "foxxxy_coxxxy", "fradtheimpaler",
+            "gatorricky", "gauravsinghmd", "glbert", "god666", "gorathger",
+            "guanm", "gucelli", "gyrating_kairos", "haggismcbean",
+            "hangingprawns", "hetraie", "hicetnunc", "hsznet", "iamjadetulip",
+            "iongler", "isaypotato", "jacobhess", "jg777", "jippiedoe",
+            "jivey", "john465", "jpokerflat", "kamekura", "kobol", "krispl",
+            "kylecuver1", "liamvo", "lichie", "loukas435", "lovlas",
+            "luvgangster", "mamaduck", "mariuseg", "matt_p_14", "mdpotts22",
+            "mingpro", "misiek91z", "mn8", "mocl125", "morallygray", "mrjv",
+            "nadgob", "nathanj439", "nimzovit", "nojero", "oddskill",
+            "odyn1982", "onbion", "pioki", "piotor", "pokerram", "prune2000",
+            "pushedpawn", "qrs", "quirked", "raitonvsfuuton", "rb52", "revhas",
+            "revoof", "rexro", "rgpchesspe", "rhohit", "robinhood76", "roso97",
+            "rpimpulse", "rsava", "s2004k1993", "scarff", "schubix", "scurly",
+            "seanysean", "sgis", "slaiyn", "slavy", "sprcow", "spuntachessts",
+            "swohl19", "tipau", "tnan123", "toj", "tomek188", "toni4127",
+            "tothecloid", "truesacrifice", "udaysatya", "vadsamoht", "vgd123",
+            "wargoblin", "yago666", "yungpirc"].iter()
+        {
+            participants.insert(unicase::Ascii::new(participant));
+        }
 
         ParticipantFilter { games: 0, matches: false, participants }
     }
