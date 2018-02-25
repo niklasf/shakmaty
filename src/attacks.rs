ba56f7eb237e81b8b3542853cc9fe6d6e3bda7b1
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-02-25 13:53:21 +0100

    add debug_assert to guard unsafe

--- src/attacks.rs
+++ src/attacks.rs
@@ -74,6 +74,7 @@ pub fn rook_attacks(sq: Square, occupied: Bitboard) -> Bitboard {
     // This is safe because a sufficient size for the attack tables was
     // hand-selected.
     let idx = (m.factor.wrapping_mul(occupied.0 & m.mask) >> (64 - 12)) as usize + m.offset;
+    debug_assert!(idx < 88772);
     Bitboard(unsafe { *ATTACKS.get_unchecked(idx) })
 }
 
@@ -91,6 +92,7 @@ pub fn bishop_attacks(sq: Square, occupied: Bitboard) -> Bitboard {
     // This is safe because a sufficient size for the attack tables was
     // hand-selected.
     let idx = (m.factor.wrapping_mul(occupied.0 & m.mask) >> (64 - 9)) as usize + m.offset;
+    debug_assert!(idx < 88772);
     Bitboard(unsafe { *ATTACKS.get_unchecked(idx) })
 }
 
