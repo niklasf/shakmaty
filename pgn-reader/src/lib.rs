007c88fdad9611b86df08f13f3f1c38d282eb87c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-05-08 21:27:38 +0200

    update shakmaty to 0.7

--- pgn-reader/src/lib.rs
+++ pgn-reader/src/lib.rs
@@ -151,7 +151,7 @@
 //! [`Visitor`]: trait.Visitor.html
 //! [Shakmaty]: ../shakmaty/index.html
 
-#![doc(html_root_url = "https://docs.rs/pgn-reader/0.5.1")]
+#![doc(html_root_url = "https://docs.rs/pgn-reader/0.6.0")]
 
 #![warn(missing_debug_implementations)]
 
