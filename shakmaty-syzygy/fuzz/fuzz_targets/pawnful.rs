801614ee70a679350273ee5a831790bf38a9f4de
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-05-31 15:25:13 +0200

    rename fuzz target

--- shakmaty-syzygy/fuzz/fuzz_targets/pawnful.rs
+++ shakmaty-syzygy/fuzz/fuzz_targets/pawnful.rs
@@ -10,7 +10,7 @@ use shakmaty::fen::Fen;
 use shakmaty_syzygy::{WdlTable, Material};
 
 fuzz_target!(|data: &[u8]| {
-    let pos: Chess = "8/2K5/8/8/8/8/3p4/1k2N3 b - - 0 1"
+    let pos: Chess = "8/2K5/8/8/8/8/3p4/1k2N3 b - - 0 1" // KNvKP
         .parse::<Fen>()
         .expect("valid fen")
         .position()
