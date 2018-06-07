918c5198289016cf8fb1ed82ea09523d8aa5fb56
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-06-07 19:13:58 +0200

    make role discriminants public

--- src/types.rs
+++ src/types.rs
@@ -148,6 +148,19 @@ impl Role {
     }
 }
 
+macro_rules! from_role_impl {
+    ($($t:ty)+) => {
+        $(impl From<Role> for $t {
+            #[inline]
+            fn from(role: Role) -> $t {
+                role as $t
+            }
+        })+
+    }
+}
+
+from_role_impl! { u8 i8 u16 i16 u32 i32 u64 i64 usize isize }
+
 pub const ROLES: [Role; 6] = [Pawn, Knight, Bishop, Rook, Queen, King];
 
 /// A piece with [`Color`] and [`Role`].
