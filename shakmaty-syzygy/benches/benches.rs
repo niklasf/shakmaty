44e447a1aed7e319e6ea714a8a4b87dc20d3c20e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-08-14 00:48:47 +0200

    benchmark add_directory

---
+++ shakmaty-syzygy/benches/benches.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate bencher;
+extern crate shakmaty;
+extern crate shakmaty_syzygy;
+
+use bencher::Bencher;
+use shakmaty::Chess;
+use shakmaty_syzygy::Tablebase;
+
+fn bench_add_directory(bench: &mut Bencher) {
+    bench.iter(|| {
+        let mut tablebase = Tablebase::<Chess>::new();
+        tablebase.add_directory("tables/regular").expect("readable directory");
+    });
+}
+
+benchmark_group!(benches,
+    bench_add_directory);
+
+benchmark_main!(benches);
