2d2973952ddfe67e7c5f637a30e5b02442bd003b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-10-08 18:00:11 +0200

    Revert "add File::upper_char()"
    This reverts commit 5c957a686697994ad775ce18973c1b660e84d471.

--- src/square.rs
+++ src/square.rs
@@ -75,11 +75,6 @@ impl File {
     }
 
     #[inline]
-    pub fn upper_char(self) -> char {
-        (b'A' + u8::from(self)) as char
-    }
-
-    #[inline]
     pub fn rotate(self) -> Rank {
         Rank::new(i8::from(self))
     }
