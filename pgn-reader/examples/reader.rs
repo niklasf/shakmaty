ed2058bd07e7811f4e75077397b613def3a55275
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-11-01 22:34:21 +0100

    skip whitespace

--- pgn-reader/examples/reader.rs
+++ pgn-reader/examples/reader.rs
@@ -2,12 +2,20 @@ extern crate pgn_reader;
 
 use std::fs::File;
 use std::env;
-use pgn_reader::reader::PgnReader;
+use pgn_reader::reader::{PgnReader, Visitor};
+
+struct MyVisitor;
+
+impl Visitor for MyVisitor {
+    type Result = ();
+
+    fn end_game(&mut self) { }
+}
 
 fn main() {
     for arg in env::args().skip(1) {
         let file = File::open(&arg).expect("fopen");
         let mut reader = PgnReader::new(file);
-        reader.read_game();
+        reader.read_game(&mut MyVisitor);
     }
 }
--- pgn-reader/src/reader.rs
+++ pgn-reader/src/reader.rs
@@ -1,6 +1,7 @@
 use super::{Nag, Outcome, RawHeader, Skip, San};
 use std::io;
 use std::io::Read;
+use std::ptr;
 use slice_deque::SliceDeque;
 
 pub trait Visitor {
@@ -22,6 +23,7 @@ pub trait Visitor {
     fn end_game(&mut self) -> Self::Result;
 }
 
+#[derive(Debug)]
 pub struct PgnReader<R> {
     inner: R,
     buffer: SliceDeque<u8>,
@@ -37,11 +39,16 @@ impl<R: Read> PgnReader<R> {
         }
     }
 
-    pub fn fill_buffer(&mut self) -> io::Result<()> {
+    pub fn into_inner(self) -> R {
+        self.inner
+    }
+
+    fn fill_buffer(&mut self) -> io::Result<()> {
         while self.buffer.len() < BUFFER_SIZE {
             unsafe {
                 let size = {
                     let remainder = self.buffer.tail_head_slice();
+                    ptr::write_bytes(remainder.as_mut_ptr(), 0, remainder.len()); // TODO
                     self.inner.read(remainder)?
                 };
 
@@ -56,30 +63,65 @@ impl<R: Read> PgnReader<R> {
         Ok(())
     }
 
-    fn skip_bom(&mut self) {
+    fn skip_bom(&mut self) -> io::Result<()> {
+        self.fill_buffer()?;
         if self.buffer.starts_with(b"\xef\xbb\xbf") {
             unsafe { self.buffer.move_head(3); }
         }
+        Ok(())
     }
 
-    /* fn skip_whitespace(&mut self) {
-        while let Some(ch) = self.buffer.pop_front() {
-            match ch {
-                b' ' | b'\t' | b'\r' => (),
-                b'\n' => {
+    fn skip_line(&mut self) -> io::Result<()> {
+        loop {
+            if let Some(pos) = memchr::memchr(b'\n', self.buffer.as_slice()) {
+                unsafe { self.buffer.move_head(pos as isize + 1); }
+                return Ok(());
+            } else {
+                self.fill_buffer()?;
+                if self.buffer.is_empty() {
+                    return Ok(());
                 }
             }
         }
-    } */
+    }
 
-    pub fn read_game<V: Visitor>(&mut self, mut visitor: V) -> io::Result<Option<V::Result>> {
-        self.skip_bom();
+    fn skip_whitespace_after_newline(&mut self) -> io::Result<()> {
+        let mut after_newline = true;
 
-        let pgn = self.buffer.as_slice();
+        loop {
+            while let Some(ch) = self.buffer.pop_front() {
+                match ch {
+                    b' ' | b'\t' | b'\r' => after_newline = false,
+                    b'\n' => after_newline = true,
+                    b'%' if after_newline => self.skip_line()?,
+                    _ => {
+                        self.buffer.push_front(ch);
+                        return Ok(());
+                    }
+                }
+            }
+
+            self.fill_buffer()?;
+            if self.buffer.is_empty() {
+                return Ok(());
+            }
+        }
+    }
+
+    pub fn read_game<V: Visitor>(&mut self, visitor: &mut V) -> io::Result<Option<V::Result>> {
+        self.skip_bom()?;
+        self.skip_whitespace_after_newline()?;
+
+        self.fill_buffer()?;
+        if self.buffer.is_empty() {
+            return Ok(None);
+        }
 
         visitor.begin_game();
         visitor.begin_headers();
+        visitor.end_headers();
 
+        self.skip_whitespace_after_newline()?;
 
         Ok(Some(visitor.end_game()))
     }
