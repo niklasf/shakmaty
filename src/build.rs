2f542206dae976ad0056a9a7d0428d1601c58886
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-15 20:51:30 +0200

    no longer need version_check

--- src/build.rs
+++ src/build.rs
@@ -92,11 +92,6 @@ fn dump_table<W: Write, T: Clone + LowerHex>(w: &mut W, name: &str, tname: &str,
 }
 
 fn main() -> io::Result<()> {
-    // detect support for nightly features
-    if let Some(true) = version_check::is_feature_flaggable() {
-        println!("cargo:rustc-cfg=nightly");
-    }
-
     // generate attacks.rs
     let out_dir = env::var("OUT_DIR").expect("got OUT_DIR");
     let dest_path = Path::new(&out_dir).join("attacks.rs");
