60c5366419b9f62b54b5132d2e8c82f1d6dc90b1
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-12-13 12:55:28 +0100

    update fuzz target

--- shakmaty-syzygy/fuzz/fuzz_targets/pawnful.rs
+++ shakmaty-syzygy/fuzz/fuzz_targets/pawnful.rs
@@ -2,7 +2,7 @@
 
 use libfuzzer_sys::fuzz_target;
 
-use shakmaty::{Chess, Setup};
+use shakmaty::{CastlingMode, Chess, Setup};
 use shakmaty::fen::Fen;
 use shakmaty_syzygy::WdlTable;
 
@@ -10,7 +10,7 @@ fuzz_target!(|data: &[u8]| {
     let pos: Chess = "8/2K5/8/8/8/8/3p4/1k2N3 b - - 0 1" // KNvKP
         .parse::<Fen>()
         .expect("valid fen")
-        .position()
+        .position(CastlingMode::Standard)
         .expect("valid position");
 
     if let Ok(table) = WdlTable::new(data, &pos.board().material()) {
