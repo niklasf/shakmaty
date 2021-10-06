0ac6577b6f34002ab7860ec02bd75d5bcb4bbd7a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-06 22:26:50 +0200

    hardcode POLYGLOT_RANDOM_ARRAY

--- src/build.rs
+++ src/build.rs
@@ -25,9 +25,6 @@ use std::io;
 use std::io::Write;
 use std::path::Path;
 
-use rand::prelude::*;
-
-
 mod color;
 mod util;
 mod types;
@@ -100,9 +97,9 @@ fn dump_slice<W: Write, T: Clone + LowerHex>(w: &mut W, name: &str, tname: &str,
     writeln!(w, "];")
 }
 
-fn dump_table<W: Write, T: Clone + LowerHex, const ROWS: usize, const COLS: usize>(w: &mut W, name: &str, tname: &str, table: &[[T; COLS]; ROWS]) -> io::Result<()> {
+fn dump_table<W: Write, T: Clone + LowerHex>(w: &mut W, name: &str, tname: &str, table: &[[T; 64]; 64]) -> io::Result<()> {
     writeln!(w, "#[allow(clippy::unreadable_literal)]")?;
-    write!(w, "static {}: [[{}; {}]; {}] = [", name, tname, COLS, ROWS)?;
+    write!(w, "static {}: [[{}; 64]; 64] = [", name, tname)?;
     for row in table.iter() {
         write!(w, "[")?;
         for column in row.iter().cloned() {
@@ -114,18 +111,12 @@ fn dump_table<W: Write, T: Clone + LowerHex, const ROWS: usize, const COLS: usiz
 }
 
 fn main() -> io::Result<()> {
-    let out_dir = env::var("OUT_DIR").expect("got OUT_DIR");
-
     // generate attacks.rs
+    let out_dir = env::var("OUT_DIR").expect("got OUT_DIR");
     let attacks_path = Path::new(&out_dir).join("attacks.rs");
     let mut f = File::create(&attacks_path).expect("created attacks.rs");
     generate_basics(&mut f)?;
-    generate_sliding_attacks(&mut f)?;
-
-    // generate zobrist.rs
-    let zobrist_path = Path::new(&out_dir).join("zobrist.rs");
-    let mut f = File::create(&zobrist_path).expect("error creating zobrist.rs");
-    generate_zobrist(&mut f)
+    generate_sliding_attacks(&mut f)
 }
 
 fn generate_basics<W: Write>(f: &mut W) -> io::Result<()> {
@@ -177,33 +168,4 @@ fn generate_sliding_attacks<W: Write>(f: &mut W) -> io::Result<()> {
     }
 
     dump_slice(f, "ATTACKS", "u64", &attacks)
-}
-
-// inspiration from this implementation taken from: https://github.com/sfleischman105/Pleco
-fn generate_zobrist<W: Write>(f: &mut W) -> io::Result<()> {
-    let seed = 0x30b3_1137_bb45_7b1b_u64;
-    let mut rnd = StdRng::seed_from_u64(seed);
-
-    let mut piece_square :[[u64; 16]; 64] = [[0; 16]; 64];
-
-    // generate random values for the piece-square table
-    for row in piece_square.iter_mut() {
-        *row = rnd.gen();
-    }
-
-    dump_table(f, "PIECE_SQUARE", "u64", &piece_square)?;
-
-    // generate random values for enpassant
-    let enpassant :[u64; 8] = rnd.gen();
-
-    dump_slice(f, "ENPASSANT", "u64", &enpassant).expect("Error dumping enpassant slice");
-
-    // generate random values for castling
-    let castle :[u64; 4] = rnd.gen();
-
-    dump_slice(f, "CASTLE", "u64", &castle).expect("Error dumping castle slice");
-
-
-    // generate two random values: side & no-pawns
-    writeln!(f, "const SIDE :u64 = 0x{:x}_u64;", rnd.gen::<u64>())
 }
