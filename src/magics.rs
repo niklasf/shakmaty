8f4c2b6953ebf04ea08c9f3dd3dac29c1ed3eb0f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-10-13 16:41:02 +0200

    let rustfmt skip magic tables

--- src/magics.rs
+++ src/magics.rs
@@ -7,6 +7,7 @@ pub struct Magic {
     pub offset: usize,
 }
 
+#[rustfmt::skip]
 pub static ROOK_MAGICS: [Magic; 64] = [
     Magic { mask: 0x0001_0101_0101_017e, factor: 0x0028_0077_ffeb_fffe, offset: 26304 },
     Magic { mask: 0x0002_0202_0202_027c, factor: 0x2004_0102_0109_7fff, offset: 35520 },
@@ -74,6 +75,7 @@ pub static ROOK_MAGICS: [Magic; 64] = [
     Magic { mask: 0x7e80_8080_8080_8000, factor: 0x0001_ffff_9dff_a333, offset: 14826 },
 ];
 
+#[rustfmt::skip]
 pub static BISHOP_MAGICS: [Magic; 64] = [
     Magic { mask: 0x0040_2010_0804_0200, factor: 0x007f_bfbf_bfbf_bfff, offset:  5378 },
     Magic { mask: 0x0000_4020_1008_0400, factor: 0x0000_a060_4010_07fc, offset:  4093 },
