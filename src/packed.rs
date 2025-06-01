7a1b0c7330719fedfd2dac6918f8e38a63af9c74
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-06-01 15:27:17 +0200

    finished setup packing

--- src/packed.rs
+++ src/packed.rs
@@ -1,7 +1,9 @@
-use crate::Setup;
+use crate::{Bitboard, Board, ByColor, ByRole, Color, Piece, Rank, Role, Setup, Square};
 use core::array::TryFromSliceError;
-use core::mem;
+use core::error;
+use core::fmt::Display;
 use core::num::NonZeroU32;
+use core::{fmt, mem};
 
 use crate::util::try_from_slice_error;
 
@@ -9,28 +11,32 @@ use crate::util::try_from_slice_error;
 //
 // 8 bytes: occupancy
 // 32 bytes: Up to 64 piece nibbles
-// 6 byte: leb128 ply
+// 5 byte: leb128 ply
 // 5 byte: leb128 half move clock
 // 1 byte: variant info
 // + 1 byte: check counts
 // + 5 bytes: pockets
 //   8 bytes: promoted flags
 // ---
-// 65 bytes
+// 64 bytes
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct PackedSetup([u8; PackedSetup::MAX_BYTES]);
+pub struct PackedSetup {
+    inner: [u8; PackedSetup::MAX_BYTES],
+}
 
 impl PackedSetup {
-    pub const MAX_BYTES: usize = 8 + 64 / 2 + 6 + 5 + 1 + 5 + 8;
+    pub const MAX_BYTES: usize = 8 + 64 / 2 + 5 + 5 + 1 + 5 + 8;
 
     pub const fn empty() -> PackedSetup {
-        PackedSetup([0; PackedSetup::MAX_BYTES])
+        PackedSetup {
+            inner: [0; PackedSetup::MAX_BYTES],
+        }
     }
 
     pub fn as_bytes(&self) -> &[u8] {
         // Trim trailing zeroes
-        let mut bytes = &self.0[..];
+        let mut bytes = &self.inner[..];
         while let [rest @ .., last] = bytes {
             if *last == 0 {
                 bytes = rest
@@ -44,23 +50,31 @@ impl PackedSetup {
     pub fn try_from_bytes(bytes: &[u8]) -> Result<PackedSetup, TryFromSliceError> {
         let mut packed = PackedSetup::empty();
         let dst = packed
-            .0
+            .inner
             .get_mut(..bytes.len())
             .ok_or_else(try_from_slice_error)?;
         dst.copy_from_slice(bytes);
         Ok(packed)
     }
 
-    pub fn pack_chess(setup: &Setup) -> PackedSetup {
+    pub fn pack_board(board: &Board) -> PackedSetup {
+        PackedSetup::pack_standard(&Setup {
+            board: board.clone(),
+            ..Setup::default()
+        })
+        .expect("all boards representable")
+    }
+
+    pub fn pack_standard(setup: &Setup) -> Result<PackedSetup, PackSetupError> {
         PackedSetup::pack_internal(setup, setup.halfmoves, setup.fullmoves, 0)
     }
 
-    pub fn pack_chess_normalized(setup: &Setup) -> PackedSetup {
+    pub fn pack_standard_normalized(setup: &Setup) -> Result<PackedSetup, PackSetupError> {
         PackedSetup::pack_internal(setup, 0, NonZeroU32::MIN, 0)
     }
 
     #[cfg(feature = "variant")]
-    pub fn pack_variant(setup: &Setup, variant: Variant) -> PackedSetup {
+    pub fn pack_variant(setup: &Setup, variant: Variant) -> Result<PackedSetup, PackSetupError> {
         PackedSetup::pack_internal(
             setup,
             setup.halfmoves,
@@ -70,7 +84,10 @@ impl PackedSetup {
     }
 
     #[cfg(feature = "variant")]
-    pub fn pack_variant_normalized(setup: &Setup, variant: Variant) -> PackedSetup {
+    pub fn pack_variant_normalized(
+        setup: &Setup,
+        variant: Variant,
+    ) -> Result<PackedSetup, PackSetupError> {
         PackedSetup::pack_internal(setup, 0, NonZerou32::MIN, variant_to_byte(variant))
     }
 
@@ -79,25 +96,246 @@ impl PackedSetup {
         halfmoves: u32,
         fullmoves: NonZeroU32,
         variant: u8,
-    ) -> PackedSetup {
-        todo!()
+    ) -> Result<PackedSetup, PackSetupError> {
+        let mut packed = PackedSetup::empty();
+        let mut writer = Writer::new(&mut packed.inner);
+
+        writer.write_u64(setup.board.occupied().into());
+
+        let mut pawn_pushed_to = setup.ep_square.map(|sq| sq.xor(Square::A2));
+        let mut unmoved_rooks = setup.castling_rights;
+        #[rustfmt::skip]
+        let mut pack_piece = |sq: Square, piece: Piece| -> u8 {
+            match piece {
+                Piece { role: Role::Pawn, color } if color == Color::from_white(sq.rank() <= Rank::Fourth) && pawn_pushed_to.take_if(|pawn| *pawn == sq).is_some() => 12,
+                Piece { role: Role::Pawn, color: Color::White } => 0,
+                Piece { role: Role::Pawn, color: Color::Black } => 1,
+                Piece { role: Role::Knight, color: Color::White } => 2,
+                Piece { role: Role::Knight, color: Color::Black } => 3,
+                Piece { role: Role::Bishop, color: Color::White } => 4,
+                Piece { role: Role::Bishop, color: Color::Black } => 5,
+                Piece { role: Role::Rook, color: Color::White } => if unmoved_rooks.remove(sq) { 13 } else { 6 },
+                Piece { role: Role::Rook, color: Color::Black } => if unmoved_rooks.remove(sq) { 14 } else { 7 },
+                Piece { role: Role::Queen, color: Color::White } => 8,
+                Piece { role: Role::Queen, color: Color::Black } => 9,
+                Piece { role: Role::King, color: Color::White } => 10,
+                Piece { role: Role::King, color: Color::Black } => setup.turn.fold_wb(11, 15),
+            }
+        };
+        let mut pieces = setup.board.iter();
+        while let Some((sq, piece)) = pieces.next() {
+            writer.write_nibbles_unchecked(
+                pack_piece(sq, piece),
+                pieces.next().map_or(0, |(sq, piece)| pack_piece(sq, piece)),
+            );
+        }
+        if pawn_pushed_to.is_some() {
+            return Err(PackSetupError::EpSquare);
+        }
+        if unmoved_rooks.any() {
+            return Err(PackSetupError::CastlingRights);
+        }
+
+        let ply = (u64::from(u32::from(fullmoves)) - 1) * 2 + setup.turn.fold_wb(0, 1);
+        let broken_turn = setup.turn.is_black()
+            && (setup.board.by_role(Role::King) & setup.board.by_color(Color::Black)).is_empty();
+
+        if halfmoves > 0 || ply > 1 || broken_turn || variant != 0 {
+            writer.write_leb128(u64::from(halfmoves));
+        }
+
+        if ply > 1 || broken_turn || variant != 0 {
+            writer.write_leb128(ply);
+        }
+
+        if variant != 0 {
+            writer.write_u8(variant);
+        }
+
+        if variant == VARIANT_CRAZYHOUSE {
+            let pockets = setup.pockets.unwrap_or_default();
+            writer
+                .write_nibbles(pockets.white.pawn, pockets.black.pawn)
+                .map_err(|()| PackSetupError::Pockets)?;
+            writer
+                .write_nibbles(pockets.white.knight, pockets.black.knight)
+                .map_err(|()| PackSetupError::Pockets)?;
+            writer
+                .write_nibbles(pockets.white.bishop, pockets.black.bishop)
+                .map_err(|()| PackSetupError::Pockets)?;
+            writer
+                .write_nibbles(pockets.white.rook, pockets.black.rook)
+                .map_err(|()| PackSetupError::Pockets)?;
+            writer
+                .write_nibbles(pockets.white.queen, pockets.black.queen)
+                .map_err(|()| PackSetupError::Pockets)?;
+            if setup.promoted.any() {
+                writer.write_u64(setup.promoted.into());
+            }
+        } else if setup.pockets.is_some() {
+            return Err(PackSetupError::Pockets);
+        }
+
+        if variant == VARIANT_THREECHECK {
+            let remaining_checks = setup.remaining_checks.unwrap_or_default();
+            writer.write_nibbles_unchecked(
+                remaining_checks.white.into(),
+                remaining_checks.black.into(),
+            );
+        } else if setup.remaining_checks.is_some() {
+            return Err(PackSetupError::RemainingChecks);
+        }
+
+        Ok(packed)
     }
 
-    pub fn unpack_standard(&self) -> Setup {
-        let (setup, _) = self.unpack_internal();
-        setup
+    pub fn unpack_board(&self) -> Result<Board, UnpackSetupError> {
+        Ok(self.unpack_standard()?.board)
+    }
+
+    pub fn unpack_standard(&self) -> Result<Setup, UnpackSetupError> {
+        let (setup, _) = self.unpack_internal()?;
+        Ok(setup)
     }
 
     #[cfg(feature = "variant")]
-    pub fn unpack_variant(&self) -> (Setup, Variant) {
-        let (setup, variant) = self.unpack_internal();
-        (setup, byte_to_variant(variant))
+    pub fn unpack_variant(&self) -> Result<(Setup, Variant), UnpackSetupError> {
+        let (setup, variant) = self.unpack_internal()?;
+        Ok((setup, byte_to_variant(variant)?))
     }
 
-    fn unpack_internal(&self) -> (Setup, u8) {
-        todo!()
+    fn unpack_internal(&self) -> Result<(Setup, u8), UnpackSetupError> {
+        let mut reader = Reader::new(&self.inner);
+
+        let mut setup = Setup::empty();
+
+        #[rustfmt::skip]
+        let mut unpack_piece = |sq: Square, packed: u8| -> Result<(), UnpackSetupError> {
+            setup.board.set_piece_at(
+                sq,
+                match packed {
+                    0 => Piece { color: Color::White, role: Role::Pawn },
+                    1 => Piece { color: Color::Black, role: Role::Pawn },
+                    2 => Piece { color: Color::White, role: Role::Knight },
+                    3 => Piece { color: Color::Black, role: Role::Knight },
+                    4 => Piece { color: Color::White, role: Role::Bishop },
+                    5 => Piece { color: Color::Black, role: Role::Bishop },
+                    6 => Piece { color: Color::White, role: Role::Rook },
+                    7 => Piece { color: Color::Black, role: Role::Rook },
+                    8 => Piece { color: Color::White, role: Role::Queen },
+                    9 => Piece { color: Color::Black, role: Role::Queen },
+                    10 => Piece { color: Color::White, role: Role::King },
+                    11 => Piece { color: Color::Black, role: Role::King },
+                    12 => {
+                        setup.ep_square = Some(sq.xor(Square::A2));
+                        Color::from_white(sq.rank() <= Rank::Fourth).pawn()
+                    },
+                    13 => {
+                        setup.castling_rights.add(sq);
+                        Piece { color: Color::White, role: Role::Rook }
+                    },
+                    14 => {
+                        setup.castling_rights.add(sq);
+                        Piece { color: Color::Black, role: Role::Rook }
+                    },
+                    15 => {
+                        setup.turn = Color::Black;
+                        Piece { color: Color::Black, role: Role::King }
+                    },
+                    _ => return Err(UnpackSetupError { _priv: () }) // Invalid packed piece
+                }
+            );
+            Ok(())
+        };
+
+        let mut occupied = Bitboard(reader.read_u64()).into_iter();
+        while let Some(sq) = occupied.next() {
+            let (lo, hi) = reader.read_nibbles();
+            unpack_piece(sq, lo)?;
+            if let Some(sq) = occupied.next() {
+                unpack_piece(sq, hi)?;
+            }
+        }
+
+        setup.halfmoves = reader
+            .read_leb128(5)
+            .try_into()
+            .map_err(|_| UnpackSetupError { _priv: () })?;
+        let ply = reader.read_leb128(5);
+        let variant = reader.read_u8();
+
+        if ply % 2 == 1 {
+            setup.turn = Color::Black;
+        }
+
+        setup.fullmoves = u32::try_from(1 + ply / 2)
+            .ok()
+            .and_then(NonZeroU32::new)
+            .ok_or(UnpackSetupError { _priv: () })?;
+
+        if variant == VARIANT_CRAZYHOUSE {
+            let (wp, bp) = reader.read_nibbles();
+            let (wn, bn) = reader.read_nibbles();
+            let (wb, bb) = reader.read_nibbles();
+            let (wr, br) = reader.read_nibbles();
+            let (wq, bq) = reader.read_nibbles();
+            setup.pockets = Some(ByColor {
+                white: ByRole {
+                    pawn: wp,
+                    knight: wn,
+                    bishop: wb,
+                    rook: wr,
+                    queen: wq,
+                    king: 0,
+                },
+                black: ByRole {
+                    pawn: bp,
+                    knight: bn,
+                    bishop: bb,
+                    rook: br,
+                    queen: bq,
+                    king: 0,
+                },
+            });
+        }
+
+        if variant == VARIANT_THREECHECK {
+            let (lo, hi) = reader.read_nibbles();
+            setup.remaining_checks = Some(ByColor {
+                white: lo.try_into().map_err(|_| UnpackSetupError { _priv: () })?,
+                black: hi.try_into().map_err(|_| UnpackSetupError { _priv: () })?,
+            });
+        }
+
+        Ok((setup, variant))
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct UnpackSetupError {
+    _priv: (),
+}
+
+#[derive(Debug, Clone)]
+pub enum PackSetupError {
+    EpSquare,
+    CastlingRights,
+    RemainingChecks,
+    Pockets,
+}
+
+impl Display for PackSetupError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(match *self {
+            PackSetupError::EpSquare => "unrepresentable ep square",
+            PackSetupError::CastlingRights => "unrepresentable castling rights",
+            PackSetupError::RemainingChecks => "unrepresentable remaining checks",
+            PackSetupError::Pockets => "unrepresentable pockets",
+        })
+    }
+}
+
+impl error::Error for PackSetupError {}
 
 impl TryFrom<&[u8]> for PackedSetup {
     type Error = TryFromSliceError;
@@ -107,12 +345,16 @@ impl TryFrom<&[u8]> for PackedSetup {
     }
 }
 
+const VARIANT_CRAZYHOUSE: u8 = 1;
+const VARIANT_THREECHECK: u8 = 5;
+
 #[cfg(feature = "variant")]
 fn variant_to_byte(variant: Variant) -> u8 {
     match variant {
         Variant::Chess => 0,
+        Variant::Crazyhouse => VARIANT_CRAZYHOUSE,
         Variant::KingOfTheHill => 4,
-        Variant::ThreeCheck => 5,
+        Variant::ThreeCheck => VARIANT_THREECHECK,
         Variant::Antichess => 6,
         Variant::Atomic => 7,
         Variant::Horde => 8,
@@ -121,16 +363,18 @@ fn variant_to_byte(variant: Variant) -> u8 {
 }
 
 #[cfg(feature = "variant")]
-fn byte_to_variant(variant: u8) -> Variant {
-    match variant {
+fn byte_to_variant(variant: u8) -> Result<Variant, UnpackSetupError> {
+    Ok(match variant {
+        0 | 2 | 3 => Variant::Chess,
+        VARIANT_CRAZYHOUSE => Variant::Crazyhouse,
         4 => Variant::KingOfTheHill,
-        5 => Variant::ThreeCheck,
+        VARIANT_THREECHECK => Variant::ThreeCheck,
         6 => Variant::Antichess,
         7 => Variant::Atomic,
         8 => Variant::Horde,
         9 => Variant::RacingKings,
-        _ => Variant::Chess,
-    }
+        _ => return Err(UnpackSetupError),
+    })
 }
 
 struct Writer<'a> {
@@ -155,16 +399,26 @@ impl Writer<'_> {
         self.inner = tail;
     }
 
-    fn write_nibbles(&mut self, lo: u8, hi: u8) {
+    #[inline]
+    fn write_nibbles_unchecked(&mut self, lo: u8, hi: u8) {
         debug_assert!(lo & 0xf == lo);
         debug_assert!(hi & 0xf == hi);
         self.write_u8(lo | (hi << 4))
     }
 
+    fn write_nibbles(&mut self, lo: u8, hi: u8) -> Result<(), ()> {
+        if lo & 0xf == lo || hi & 0xf == hi {
+            self.write_nibbles_unchecked(lo, hi);
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
     fn write_leb128(&mut self, mut n: u64) {
         while n > 127 {
             self.write_u8(n as u8 | 128);
-            n = n >> 7;
+            n >>= 7;
         }
         self.write_u8(n as u8);
     }
