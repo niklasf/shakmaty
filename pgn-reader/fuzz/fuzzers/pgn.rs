893ba1e43096f414d0d5b01126bcdc000c0890f6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-06-15 23:50:07 +0200

    fixup fuzz target

--- pgn-reader/fuzz/fuzzers/pgn.rs
+++ pgn-reader/fuzz/fuzzers/pgn.rs
@@ -3,15 +3,16 @@
 use libfuzzer_sys::fuzz_target;
 
 use pgn_reader::{BufferedReader, Visitor};
+use std::io::Cursor;
 
 struct MyVisitor;
 
 impl Visitor for MyVisitor {
     type Result = ();
-    fn end_game(&mut self) { }
+    fn end_game(&mut self) {}
 }
 
 fuzz_target!(|data: &[u8]| {
     let mut visitor = MyVisitor;
-    let _ = BufferedReader::new_cursor(data).read_all(&mut visitor);
+    let _ = BufferedReader::new(Cursor::new(data)).read_all(&mut visitor);
 });
