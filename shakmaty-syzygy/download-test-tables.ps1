4bd56af8b3df658da194d78439b82e8bf8de9d84
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-06-23 23:32:13 +0200

    try bash in windows ci

--- shakmaty-syzygy/download-test-tables.ps1
+++
@@ -1,3 +0,0 @@
-cd tables\chess; wget -w 1 -nc -i TEST-SOURCE.txt; cd ..\..
-cd tables\atomic; wget -w 1 -nc -i TEST-SOURCE.txt; cd ..\..
-cd tables\antichess; wget -w 1 -nc -i TEST-SOURCE.txt; cd ..\..
