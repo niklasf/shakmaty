2f4c5b19a4c13f44bf2acd253011e2686cebeab0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-07-04 15:52:55 +0200

    pgn-reader: tweak Visitor docs

--- pgn-reader/src/visitor.rs
+++ pgn-reader/src/visitor.rs
@@ -7,31 +7,40 @@ pub struct Skip(pub bool);
 
 /// Consumes games from a reader.
 ///
-/// ![Flow](https://github.com/niklasf/rust-pgn-reader/blob/master/docs/visitor.png?raw=true)
+/// ![Flow](https://github.com/niklasf/shakmaty/blob/master/pgn-reader/docs/visitor.png?raw=true)
 pub trait Visitor {
     /// Value produced by the visitor after reading a game.
     type Result;
 
-    /// Called at the start of the game; directly before reading game tags.
+    /// Called at the start of the game, directly before reading game tags.
     fn begin_tags(&mut self) {}
-    /// Called when parsing a game tag pair like `[White "Deep Blue"]`.
-    fn tag(&mut self, _name: &[u8], _value: RawTag<'_>) {}
-    /// Called after reading the tags of a game; before reading the movetext.
+    /// Called when parsing a game tag pair, like `[White "Deep Blue"]`.
+    fn tag(&mut self, name: &[u8], value: RawTag<'_>) {
+        let _name = name;
+        let _value = value;
+    }
+    /// Called after reading the tags of a game, before reading the movetext.
     /// May skip over the following movetext directly to
-    /// [`end_game()`](trait.Visitor.html#tymethod.end_game).
+    /// [`Visitor::end_game()`].
     fn begin_movetext(&mut self) -> Skip {
         Skip(false)
     }
 
     /// Called for each move, like `Nf3+`.
-    fn san(&mut self, _san_plus: SanPlus) {}
-    /// Called for each numeric annotation glyph like `!?` or `$7`.
-    fn nag(&mut self, _nag: Nag) {}
+    fn san(&mut self, san_plus: SanPlus) {
+        let _san_plus = san_plus;
+    }
+    /// Called for each numeric annotation glyph, like `!?` or `$7`.
+    fn nag(&mut self, nag: Nag) {
+        let _nag = nag;
+    }
     /// Called for each `{ comment }`.
-    fn comment(&mut self, _comment: RawComment<'_>) {}
+    fn comment(&mut self, comment: RawComment<'_>) {
+        let _comment = comment;
+    }
     /// Called for each `(`. May skip over the following variation directly
-    /// to [`end_variation()`](trait.Visitor.html#method.end_variation) (or to
-    /// [`end_game()`](trait.Visitor.html#tymethod.end_game) if no matching `)`
+    /// to [`Visitor::end_variation()`] (or to
+    /// [`Visitor::end_game()`] if no matching `)`
     /// follows before the end of the game.
     fn begin_variation(&mut self) -> Skip {
         Skip(false)
@@ -40,7 +49,9 @@ pub trait Visitor {
     /// matching `(`.
     fn end_variation(&mut self) {}
     /// Called for each game termination, like `*` or `1-0`.
-    fn outcome(&mut self, _outcome: Outcome) {}
+    fn outcome(&mut self, outcome: Outcome) {
+        let _outcome = outcome;
+    }
 
     /// Called after parsing a game. Can produce a custom result.
     fn end_game(&mut self) -> Self::Result;
